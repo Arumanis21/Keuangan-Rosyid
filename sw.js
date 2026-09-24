bc111d4047fb7ad667d0a1c3107817bf4c87e0b4
Arumanis21 <rosyids135@gmail.com>
2026-09-24 15:31:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
-const CACHE_NAME = 'keuangan-rosyid-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'keuangan-rosyid-v3'; 
+const ASSETS = [
+  './', 
+  './index.html', 
+  './manifest.json'
+];
 
+// Tahap Install: Simpan aset ke cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Tahap Activate: Bersihkan cache lama agar kode baru langsung jalan
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Tahap Fetch: Tangani akses offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
